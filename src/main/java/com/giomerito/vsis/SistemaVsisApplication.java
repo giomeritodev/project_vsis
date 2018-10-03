e4fbe9316d471362325e615d26aa61bcbffdcb03
Giomerito Alves de Souza <giomerito.dev@gmail.com>
2018-10-02 22:41:43 -0300

    Projeto criado

---
+++ src/main/java/com/giomerito/vsis/SistemaVsisApplication.java
@@ -0,0 +1,12 @@
+package com.giomerito.vsis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaVsisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaVsisApplication.class, args);
+	}
+}
