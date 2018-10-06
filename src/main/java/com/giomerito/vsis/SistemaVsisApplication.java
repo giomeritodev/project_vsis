a7b343686ddd211a58ec5cad23e23f5436c4e20b
Giomerito Alves de Souza <giomerito.dev@gmail.com>
2018-10-05 21:08:45 -0300

    Criando operacao de instanciacao

--- src/main/java/com/giomerito/vsis/SistemaVsisApplication.java
+++ src/main/java/com/giomerito/vsis/SistemaVsisApplication.java
@@ -1,12 +1,31 @@
 package com.giomerito.vsis;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SistemaVsisApplication {
+import com.giomerito.vsis.domain.Categoria;
+import com.giomerito.vsis.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class SistemaVsisApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaVsisApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Sorvete");
+		Categoria cat2 = new Categoria(null, "Picolé");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
