58d32c097bc1bb178c434bad57ab42a4fc5347d1
Giomerito Alves de Souza <giomerito.dev@gmail.com>
2018-10-15 22:20:21 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/giomerito/vsis/SistemaVsisApplication.java
+++ src/main/java/com/giomerito/vsis/SistemaVsisApplication.java
@@ -115,12 +115,12 @@ public class SistemaVsisApplication implements CommandLineRunner{
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		
-		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"),cli1, end1);
-		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"),cli1, end2);
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, end1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, end2);
 		
 		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
 		ped1.setPagamento(pagto1);
-		Pagamento pagto2 = new PagamentoComDinheiro(null, EstadoPagamento.QUITADO, ped2, 120.00);
+		Pagamento pagto2 = new PagamentoComDinheiro(null, EstadoPagamento.QUITADO, ped2, 700.00);
 		ped2.setPagamento(pagto2);
 		
 		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
---
+++ src/main/java/com/giomerito/vsis/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.giomerito.vsis.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.giomerito.vsis.domain.Pedido;
+import com.giomerito.vsis.repositories.PedidoRepository;
+import com.giomerito.vsis.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado, ID: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
