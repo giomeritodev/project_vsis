20eff7ad7a821c9e2d32ab524016b4be3c47a47d
Giomerito Alves de Souza <giomerito.dev@gmail.com>
2018-11-10 23:28:31 -0300

    PUT, DELETE, UPDATE para Cliente

--- src/main/java/com/giomerito/vsis/services/ClienteService.java
+++ src/main/java/com/giomerito/vsis/services/ClienteService.java
@@ -1,13 +1,20 @@
 package com.giomerito.vsis.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.giomerito.vsis.domain.Categoria;
 import com.giomerito.vsis.domain.Cliente;
+import com.giomerito.vsis.dto.ClienteDTO;
 import com.giomerito.vsis.repositories.ClienteRepository;
+import com.giomerito.vsis.services.exceptions.DataIntegrityException;
 import com.giomerito.vsis.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -20,5 +27,40 @@ public class ClienteService {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado, ID: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	//Metodo para alterar um cliente
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir cliente, existem dados relacionados!");
+		}
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	//Metodo para paginação
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String direction, String orderBy){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 }
