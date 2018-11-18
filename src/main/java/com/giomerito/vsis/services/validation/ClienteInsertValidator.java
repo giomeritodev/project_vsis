c3cdcafa861203a7fa355378a091e4f633cfb94f
Giomerito Alves de Souza <giomerito.dev@gmail.com>
2018-11-18 14:14:26 -0300

    Validacao customizada email não repetido na insercao do Cliente

--- src/main/java/com/giomerito/vsis/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/giomerito/vsis/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.giomerito.vsis.domain.Cliente;
 import com.giomerito.vsis.domain.enums.TipoCliente;
 import com.giomerito.vsis.dto.ClienteNewDTO;
+import com.giomerito.vsis.repositories.ClienteRepository;
 import com.giomerito.vsis.resources.exception.FieldMessage;
 import com.giomerito.vsis.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository cliRepo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -20,12 +28,20 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
 
+		//Validação do CPF
 		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
 		}
-
+		
+		//Validação do CNPJ
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		//Validação do Email do Cliente não pode ser repetido
+		Cliente aux = cliRepo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 
 
