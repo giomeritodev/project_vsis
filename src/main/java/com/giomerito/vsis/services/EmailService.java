31681bc640879fa4d34aef3eed9d1d474f2056ac
Giomerito Alves de Souza <giomerito.dev@gmail.com>
2018-11-22 20:12:36 -0300

    Email Html

--- src/main/java/com/giomerito/vsis/services/EmailService.java
+++ src/main/java/com/giomerito/vsis/services/EmailService.java
@@ -1,12 +1,18 @@
 package com.giomerito.vsis.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.giomerito.vsis.domain.Pedido;
 
 public interface EmailService {
-	
+
 	void sendOrderConfirmationEmail(Pedido obj);
-	
+
 	void sendEmail(SimpleMailMessage msg);
+
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+	void sendHtmlEmail(MimeMessage msg);
 }
